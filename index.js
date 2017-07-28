d87c9357d8def32a041568867f8da55d3423ada3
Michelle Li <limichelle21@gmail.com>
2017-07-27 22:10:12 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+var itemPrice = document.getElementById("price")
+var hourlyWage = document.getElementById("wage")
+
+function calculateCost() = {
+  var cost = itemPrice / hourlyWage;
+  docment.getElementById("cost").innerHTML=cost;
+}
