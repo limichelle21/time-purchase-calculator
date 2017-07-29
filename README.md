993efffeb942db88cbb9c0e263d5cc94aef09e2d
Michelle Li <limichelle21@gmail.com>
2017-07-28 21:02:47 -0700

    add README, working JS function

---
+++ README.md
@@ -0,0 +1,14 @@
+# Time is money - have you considered the time cost of your spending habits?
+
+I wanted to reframe how I thought about making purchases, from a Philz coffee to a new travel bag, and consider the cost in terms of time as opposed to dollars.
+
+This calculator takes an item price and your average hourly wage to find how much an item costs in time.
+
+Example:
+*Item*: Osprey Porter 30 Travel pack - MSRP $130
+*Hourly Wage*: $30
+*Cost of Purchase in Time*: 4.33 hours
+
+Would you be willing to work x-amount of time in order to make that purchase?
+
+Styled with Bootstrap
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-
-
-var itemPrice = document.getElementById("price")
-var hourlyWage = document.getElementById("wage")
-
-function calculateCost() = {
+function calculateCost() {
+  var itemPrice = document.getElementById('price').value;
+  var hourlyWage = document.getElementById('wage').value;
   var cost = itemPrice / hourlyWage;
-  docment.getElementById("cost").innerHTML=cost;
+  var costEl = document.getElementById('cost');
+  document.getElementById('cost').innerHTML = cost.toFixed(2) + " hours";
+  costEl.style.display = 'block';
+}
+
+function hideCost() {
+  var costEl = document.getElementById('cost');
+  costEl.style.display = 'none';
 }
